7f65048929ecda3c55e14863fa0bef543ec04492
kunalpjan <kunalpjan.work@gmail.com>
2026-01-14 12:25:20 +0530

    Update vite.config.ts
    Add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: "/prithvico/",
   server: {
     host: "::",
     port: 8080,
